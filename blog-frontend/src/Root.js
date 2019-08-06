eb5963ece2cb70fdec2a0a5d6244a5b9e4a6fa58
WhoJung Kim <wj0415@korea.ac.kr>
2019-08-06 17:06:15 +0900

    Footer Component&Button&Middle 생성 기본 User Interface 생성

--- blog-frontend/src/Root.js
+++ blog-frontend/src/Root.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "components/App";
-import configure from "./store/configure";
+import configure from "store/configure";
 import { Provider } from "react-redux";
 
 const store = configure();
--- blog-frontend/src/components/common/Header/Header.js
+++ blog-frontend/src/components/common/Header/Header.js
@@ -2,7 +2,10 @@ import React from "react";
 import styles from "./Header.scss";
 import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
+import Button from "components/common/Button";
+
 const cx = classNames.bind(styles);
+
 const Header = () => {
   return (
     <header className={cx("header")}>
@@ -10,7 +13,11 @@ const Header = () => {
         <div className={cx("brand")}>
           <Link to="/">reactblog</Link>
         </div>
-        <div className={cx("right")}>{/*조건에 따라 버튼 렌더링 */} 오른쪽</div>
+        <div className={cx("right")}>
+          <Button theme="outline" to="/editor">
+            새 포스트
+          </Button>
+        </div>
       </div>
     </header>
   );

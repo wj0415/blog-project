73b5dbba39b1ccacc2cdc5d951250e50b3f9486e
WhoJung Kim <wj0415@korea.ac.kr>
2019-08-07 19:01:18 +0900

    PostPage(Info&Body UI)  finish

--- blog-frontend/src/pages/PostPage.js
+++ blog-frontend/src/pages/PostPage.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import PostInfo from "components/post/PostInfo";
+import PageTemplate from "components/common/PageTemplate";
+import PostBody from "../components/post/PostBody/PostBody";
 const PostPage = () => {
-  return <div>Post</div>;
+  return (
+    <PageTemplate>
+      <PostInfo />
+      <PostBody />
+    </PageTemplate>
+  );
 };
 
 export default PostPage;
